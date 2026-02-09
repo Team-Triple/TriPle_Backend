78d89f959028c17a0cee2b6a02de427ad3abe91e
sangyunpark99 <psangyun5108@gmail.com>
2026-02-09 16:50:25 +0900

    [#3] feat: 결제 상태 부분 결제 제거

--- src/main/java/org/triple/backend/payment/entity/PaymentStatus.java
+++ src/main/java/org/triple/backend/payment/entity/PaymentStatus.java
@@ -1,5 +1,5 @@
 package org.triple.backend.payment.entity;
 
 public enum PaymentStatus {
-    READY, IN_PROGRESS, DONE, CANCELED, PARTIAL_CANCELED, ABORTED, EXPIRED
+    READY, IN_PROGRESS, DONE, CANCELED, ABORTED, EXPIRED
 }
