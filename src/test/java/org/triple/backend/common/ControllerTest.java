e5d37ee428f3efa4e20e22349125edf67bc658ac
sangyunpark99 <psangyun5108@gmail.com>
2026-02-10 17:38:32 +0900

    [#9] test: 공통으로 사용되는 ControllerTest 구현

--- src/test/java/org/triple/backend/common/ControllerTest.java
+++ src/test/java/org/triple/backend/common/ControllerTest.java
@@ -1,29 +1,32 @@
-package org.triple.backend.user;
+package org.triple.backend.common;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.triple.backend.user.controller.UserController;
+import org.triple.backend.user.service.UserService;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 @ExtendWith(RestDocumentationExtension.class)
-public class UserControllerTest {
+@ActiveProfiles("test")
+public abstract class ControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    protected MockMvc mockMvc;
+
+    @MockitoBean
+    protected UserService userService;
 
     @BeforeEach
     void setup(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
@@ -33,12 +36,5 @@ public class UserControllerTest {
                         .withRequestDefaults(prettyPrint())
                         .withResponseDefaults(prettyPrint()))
                 .build();
-    }
-
-    @Test
-    void apiDocsTest() throws Exception {
-        mockMvc.perform(get("/test"))
-                .andExpect(status().isOk())
-                .andDo(document("user"));
     }
 }
