9176d276a17bbdfc935c2a4485f6e83881526b1c
sangyunpark99 <psangyun5108@gmail.com>
2026-02-12 17:00:47 +0900

    [#12] chore: LoginUserArgument session 패키지 내부로 이동

--- src/main/java/org/triple/backend/global/config/WebConfig.java
+++ src/main/java/org/triple/backend/global/config/WebConfig.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.triple.backend.auth.LoginUserArgumentResolver;
+import org.triple.backend.auth.session.LoginUserArgumentResolver;
 
 import java.util.List;
 
