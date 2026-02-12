f7e5cf593cebde81b18e76bba349d73feaba4cd2
sangyunpark99 <psangyun5108@gmail.com>
2026-02-12 17:01:07 +0900

    [#12] test: SessionManger import 하도록 구현

--- src/test/java/org/triple/backend/common/annotation/WebMvcDocsTest.java
+++ src/test/java/org/triple/backend/common/annotation/WebMvcDocsTest.java
@@ -2,8 +2,10 @@ package org.triple.backend.common.annotation;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.ActiveProfiles;
+import org.triple.backend.auth.session.SessionManager;
 
 import java.lang.annotation.*;
 
@@ -11,6 +13,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @ExtendWith(RestDocumentationExtension.class)
+@Import(SessionManager.class)
 @WebMvcTest
 @ActiveProfiles("test")
 public @interface WebMvcDocsTest {
