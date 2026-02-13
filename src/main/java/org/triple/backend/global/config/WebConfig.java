10eea2a418bf949774ebb2626d43099085c42e7e
sangyunpark99 <psangyun5108@gmail.com>
2026-02-13 15:17:47 +0900

    [#28] feat: cors 허용 url 추가
    - 프론트 로컬 url
    - 프론트 배포 url

--- src/main/java/org/triple/backend/global/config/WebConfig.java
+++ src/main/java/org/triple/backend/global/config/WebConfig.java
@@ -3,6 +3,7 @@ package org.triple.backend.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.triple.backend.auth.session.LoginUserArgumentResolver;
 
@@ -13,6 +14,15 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoginUserArgumentResolver loginArgumentResolver;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://localhost:3000","https://triple.io.kr")
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
