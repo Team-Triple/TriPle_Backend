2701a927412fc35a9db76cc7de87a58915f637f5
sangyunpark99 <psangyun5108@gmail.com>
2026-02-13 18:04:22 +0900

    [#20] feat: 그룹 생성 비즈니스 로직 구현

--- src/main/java/org/triple/backend/group/service/GroupService.java
+++ src/main/java/org/triple/backend/group/service/GroupService.java
@@ -3,19 +3,28 @@ package org.triple.backend.group.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.triple.backend.global.error.BusinessException;
 import org.triple.backend.group.dto.request.CreateGroupRequestDto;
 import org.triple.backend.group.dto.response.CreateGroupResponseDto;
 import org.triple.backend.group.entity.group.Group;
+import org.triple.backend.group.entity.userGroup.Role;
 import org.triple.backend.group.repository.GroupJpaRepository;
+import org.triple.backend.user.entity.User;
+import org.triple.backend.user.exception.UserErrorCode;
+import org.triple.backend.user.repository.UserJpaRepository;
 
 @Service
 @RequiredArgsConstructor
 public class GroupService {
 
     private final GroupJpaRepository groupJpaRepository;
+    private final UserJpaRepository userJpaRepository;
 
     @Transactional
-    public CreateGroupResponseDto create(final CreateGroupRequestDto createGroupRequestDto) {
+    public CreateGroupResponseDto create(final CreateGroupRequestDto createGroupRequestDto, final Long userId) {
+
+        User user = userJpaRepository.findById(userId).orElseThrow(() -> new BusinessException(UserErrorCode.USER_NOT_FOUND));
+
         Group group = Group.builder()
                 .name(createGroupRequestDto.name())
                 .description(createGroupRequestDto.description())
@@ -24,8 +33,9 @@ public class GroupService {
                 .thumbNailUrl(createGroupRequestDto.thumbNailUrl())
                 .build();
 
+        group.addMember(user, Role.OWNER);
         Group savedGroup = groupJpaRepository.save(group);
 
-        return CreateGroupResponseDto.fromEntity(savedGroup);
+        return new CreateGroupResponseDto(savedGroup.getId());
     }
 }
