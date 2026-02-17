b0db166d5308a489e1a47803215f2e73872880c4
chungjeongsu <wjdwlgh2000@gmail.com>
2026-02-18 03:15:51 +0900

    [#47] feat: 여행 일정 생성 권한 확인위한 쿼리 추가

--- src/main/java/org/triple/backend/group/repository/UserGroupJpaRepository.java
+++ src/main/java/org/triple/backend/group/repository/UserGroupJpaRepository.java
@@ -1,7 +1,10 @@
 package org.triple.backend.group.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.triple.backend.group.entity.group.Group;
 import org.triple.backend.group.entity.userGroup.UserGroup;
+import org.triple.backend.user.entity.User;
 
 public interface UserGroupJpaRepository extends JpaRepository<UserGroup, Long> {
+    boolean existsByUserAndGroup(User user, Group group);
 }
