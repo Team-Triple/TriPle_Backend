45806cc8be1d6b8b6820a7efa56e7f32aa2d0da5
sangyunpark99 <psangyun5108@gmail.com>
2026-02-19 15:11:04 +0900

    [#56] test: Group 에러 코드 구현

---
+++ src/main/java/org/triple/backend/group/exception/GroupErrorCode.java
@@ -0,0 +1,27 @@
+package org.triple.backend.group.exception;
+
+import org.springframework.http.HttpStatus;
+import org.triple.backend.global.error.ErrorCode;
+
+public enum GroupErrorCode implements ErrorCode {
+
+    GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 그룹 입니다.");
+
+    private HttpStatus status;
+    private String message;
+
+    GroupErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    @Override
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
