e702a7bd0a808d264f2540777829822ae29a9799
sangyunpark99 <psangyun5108@gmail.com>
2026-02-19 09:38:19 +0900

    [#38] feat: 그룹 엔티티 정적 팩터리 메서드 및 검증 로직 구현

--- src/main/java/org/triple/backend/group/entity/group/Group.java
+++ src/main/java/org/triple/backend/group/entity/group/Group.java
@@ -1,10 +1,7 @@
 package org.triple.backend.group.entity.group;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.triple.backend.global.common.BaseEntity;
 import org.triple.backend.group.entity.joinApply.JoinApply;
 import org.triple.backend.group.entity.userGroup.JoinStatus;
@@ -19,9 +16,9 @@ import java.util.List;
 @Getter
 @Entity
 @Table(name = "travel_group")
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Group extends BaseEntity {
 
     @Id
@@ -32,12 +29,12 @@ public class Group extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private GroupKind groupKind;
 
-    @OneToMany(mappedBy = "group", cascade = CascadeType.PERSIST, orphanRemoval = true)
     @Builder.Default
+    @OneToMany(mappedBy = "group", cascade = CascadeType.PERSIST, orphanRemoval = true)
     private List<UserGroup> userGroups = new ArrayList<>();
 
-    @OneToMany(mappedBy = "group")
     @Builder.Default
+    @OneToMany(mappedBy = "group")
     private List<JoinApply> joinApplies = new ArrayList<>();
 
     private String name;
@@ -62,5 +59,36 @@ public class Group extends BaseEntity {
 
         this.userGroups.add(userGroup);
         user.getUserGroups().add(userGroup);
+    }
+
+    public static Group create(final GroupKind kind, final String name, final String description, final String thumbNailUrl, int memberLimit) {
+        return Group.builder()
+                .name(validateName(name))
+                .description(validateDescription(description))
+                .thumbNailUrl(thumbNailUrl)
+                .groupKind(validateKind(kind))
+                .memberLimit(validateMemberLimit(memberLimit))
+                .currentMemberCount(1)
+                .build();
+    }
+
+    private static GroupKind validateKind(final GroupKind kind) {
+        if (kind == null) throw new IllegalArgumentException("그룹 종류는 null값일 수 없습니다.");
+        return kind;
+    }
+
+    private static String validateDescription(final String description) {
+        if(description == null || description.isBlank()) throw new IllegalArgumentException("그룹 설명은 null값 혹은 빈값일 수 없습니다.");
+        return description;
+    }
+
+    private static String validateName(final String name) {
+        if (name == null || name.isBlank()) throw new IllegalArgumentException("그룹 이름은 null값 혹은 빈값일 수 없습니다.");
+        return name;
+    }
+
+    private static int validateMemberLimit(final int memberLimit) {
+        if (memberLimit < 1 || memberLimit > 50) throw new IllegalArgumentException("그룹 멤버 제한 수는 1명 이하 혹은 50명 이상이 될 수 없습니다.");
+        return memberLimit;
     }
 }
