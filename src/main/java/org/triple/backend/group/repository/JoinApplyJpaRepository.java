28f3b5c4c9955b909fc203e56321936550df9b99
sangyunpark99 <psangyun5108@gmail.com>
2026-02-20 13:46:28 +0900

    [#63] feat: 그룹 id와 사용자 id로 가입 승인 엔티티를 조회하는 로직 구현

--- src/main/java/org/triple/backend/group/repository/JoinApplyJpaRepository.java
+++ src/main/java/org/triple/backend/group/repository/JoinApplyJpaRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.triple.backend.group.entity.joinApply.JoinApply;
 
+import java.util.Optional;
+
 public interface JoinApplyJpaRepository extends JpaRepository<JoinApply, Long> {
+
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("DELETE FROM JoinApply ja WHERE ja.group.id = :groupId")
     int bulkDeleteByGroupId(Long groupId);
+
+    Optional<JoinApply> findByGroupIdAndUserId(Long groupId, Long userId);
 }
