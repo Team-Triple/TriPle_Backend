4e168d23fa20c7f6bc60b77b7686db3ca6667398
sangyunpark99 <psangyun5108@gmail.com>
2026-02-20 10:16:34 +0900

    [#61] feat: GroupErrorCode 동시 업데이트 예외 에러 코드 구현

--- src/main/java/org/triple/backend/group/exception/GroupErrorCode.java
+++ src/main/java/org/triple/backend/group/exception/GroupErrorCode.java
@@ -6,7 +6,8 @@ import org.triple.backend.global.error.ErrorCode;
 public enum GroupErrorCode implements ErrorCode {
 
     GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 그룹 입니다."),
-    NOT_GROUP_OWNER(HttpStatus.FORBIDDEN, "그룹 삭제 권한이 없습니다.");
+    NOT_GROUP_OWNER(HttpStatus.FORBIDDEN, "그룹 수정/삭제 권한이 없습니다."),
+    CONCURRENT_GROUP_UPDATE(HttpStatus.CONFLICT, "동시에 그룹 정보가 변경되었습니다. 다시 시도해주세요.");
 
     private HttpStatus status;
     private String message;
