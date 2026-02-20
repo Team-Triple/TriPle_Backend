78aff28fd1ed9cd9510ba151f8479f4bbf734aab
sangyunpark99 <psangyun5108@gmail.com>
2026-02-20 21:15:20 +0900

    [#67] feat: 그룹 세부 내역 조회 API 레포지토리 구현
    - User 엔티티의 N+1 문제를 방지하고자 fetch join을 활용했습니다.
    
    - 그룹 아이디, 사용자 아이디, 가입 상태를 조건으로 존재 여부를 파악하는 레포지토리를 구현했습니다.

--- src/main/java/org/triple/backend/group/repository/UserGroupJpaRepository.java
+++ src/main/java/org/triple/backend/group/repository/UserGroupJpaRepository.java
@@ -7,7 +7,14 @@ import org.triple.backend.group.entity.userGroup.JoinStatus;
 import org.triple.backend.group.entity.userGroup.Role;
 import org.triple.backend.group.entity.userGroup.UserGroup;
 
+import java.util.List;
+
 public interface UserGroupJpaRepository extends JpaRepository<UserGroup, Long> {
+
+    @Query("SELECT ug FROM UserGroup ug join fetch ug.user WHERE ug.group.id = :groupId AND ug.joinStatus = :joinStatus")
+    List<UserGroup> findAllByGroupIdAndJoinStatus(Long groupId, JoinStatus joinStatus);
+
+    boolean existsByGroupIdAndUserIdAndJoinStatus(Long groupId, Long userId, JoinStatus joinStatus);
 
     boolean existsByGroupIdAndUserIdAndRole(Long groupId, Long userId, Role role);
 
