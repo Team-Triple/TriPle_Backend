7b042044f158144e1ad09e1dff5df9c173b0c333
sangyunpark99 <psangyun5108@gmail.com>
2026-02-22 21:24:55 +0900

    [#69] feat: UserGroup 엔티티 조회 메서드 구현
    - 그룹 아이디, 사용자 아이디 조건을 통해 조회합니다.

--- src/main/java/org/triple/backend/group/repository/UserGroupJpaRepository.java
+++ src/main/java/org/triple/backend/group/repository/UserGroupJpaRepository.java
@@ -8,6 +8,7 @@ import org.triple.backend.group.entity.userGroup.Role;
 import org.triple.backend.group.entity.userGroup.UserGroup;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserGroupJpaRepository extends JpaRepository<UserGroup, Long> {
 
@@ -17,6 +18,8 @@ public interface UserGroupJpaRepository extends JpaRepository<UserGroup, Long> {
     boolean existsByGroupIdAndUserIdAndJoinStatus(Long groupId, Long userId, JoinStatus joinStatus);
 
     boolean existsByGroupIdAndUserIdAndRole(Long groupId, Long userId, Role role);
+
+    Optional<UserGroup> findByGroupIdAndUserId(Long groupId, Long userId);
 
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("DELETE FROM UserGroup ug WHERE ug.group.id = :groupId")
