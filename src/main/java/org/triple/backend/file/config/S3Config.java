e06cd18239fe0a92b4c59f6496615bc1d376fe1f
chungjeongsu <wjdwlgh2000@gmail.com>
2026-02-23 04:26:33 +0900

    [#57] feat: S3Config 구현

---
+++ src/main/java/org/triple/backend/file/config/S3Config.java
@@ -0,0 +1,34 @@
+package org.triple.backend.file.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.triple.backend.file.repository.FileJpaRepository;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(S3BucketProperties.class)
+public class S3Config {
+    private static final Region REGION = Region.AP_NORTHEAST_2;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(REGION)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(REGION)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+}
