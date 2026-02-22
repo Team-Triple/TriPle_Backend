0975a2b5704edb0d89c2b8e25d9cda0a3f398a17
chungjeongsu <wjdwlgh2000@gmail.com>
2026-02-23 04:30:27 +0900

    [#57] feat: 업로드 시 예외 구현
    - CopyFailedException : S3 CopyObject 실패 시
    - DeleteFailedException : S3 deleteObject 실패 시
    - InvalidKeyException : 버킷 키가 올바르지 않을 시
    - UploadFailedException : 클라이언트의 업로드가 잘 안됐을 시
    - FinalizeUploadException : 위의 예외의 최상위 예외

---
+++ src/main/java/org/triple/backend/file/infra/exception/FinalizeUploadException.java
@@ -0,0 +1,19 @@
+package org.triple.backend.file.infra.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class FinalizeUploadException extends RuntimeException {
+    private final HttpStatus httpStatus;
+
+    public FinalizeUploadException(HttpStatus httpStatus, String message) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+
+    public FinalizeUploadException(HttpStatus httpStatus, String message, Throwable cause) {
+        super(message, cause);
+        this.httpStatus = httpStatus;
+    }
+}
