79401244a534ce5b004706cd91293b697afa7d54
chungjeongsu <wjdwlgh2000@gmail.com>
2026-02-24 02:24:20 +0900

    [#73] feat: 여행 일정 업데이트 API repository 구현

---
+++ src/main/java/org/triple/backend/travel/repository/TravelItineraryJpaRepository.java
@@ -0,0 +1,13 @@
+package org.triple.backend.travel.repository;
+
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.triple.backend.travel.entity.TravelItinerary;
+
+import java.util.Optional;
+
+public interface TravelItineraryJpaRepository extends JpaRepository<TravelItinerary, Long> {
+    Optional<TravelItinerary> findById(Long travelId);
+}
---
+++ src/main/java/org/triple/backend/travel/repository/UserTravelItineraryJpaRepository.java
@@ -0,0 +1,11 @@
+package org.triple.backend.travel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.triple.backend.travel.entity.UserTravelItinerary;
+
+import java.util.Optional;
+
+public interface UserTravelItineraryJpaRepository extends JpaRepository<UserTravelItinerary, Long> {
+
+    Optional<UserTravelItinerary> findByUserIdAndTravelItineraryId(Long userId, Long travelItineraryId);
+}
