1b81946bf6d9ffc31f84e879f66de8a92def1561
sangyunpark99 <psangyun5108@gmail.com>
2026-02-23 12:40:28 +0900

    [#71] feat: INVALID_SEARCH_KEYWORD_LENGTH message 내용 변경

--- src/main/java/org/triple/backend/group/exception/GroupErrorCode.java
+++ src/main/java/org/triple/backend/group/exception/GroupErrorCode.java
@@ -8,7 +8,7 @@ public enum GroupErrorCode implements ErrorCode {
     GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 그룹 입니다."),
     NOT_GROUP_OWNER(HttpStatus.FORBIDDEN, "그룹 수정/삭제 권한이 없습니다."),
     CONCURRENT_GROUP_UPDATE(HttpStatus.CONFLICT, "동시에 그룹 정보가 변경되었습니다. 다시 시도해주세요."),
-    INVALID_SEARCH_KEYWORD_LENGTH(HttpStatus.BAD_REQUEST, "검색어는 최대 20자까지 입력할 수 있습니다.");
+    INVALID_SEARCH_KEYWORD_LENGTH(HttpStatus.BAD_REQUEST, "검색어는 최대 입력 문자 수를 초과했습니다.");
 
     private HttpStatus status;
     private String message;
