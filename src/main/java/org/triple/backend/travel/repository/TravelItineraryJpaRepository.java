f7dcae7f5948eed1620ce38a7f41c34c6cd9caa1
chungjeongsu <wjdwlgh2000@gmail.com>
2026-02-24 17:33:18 +0900

    [#78] feat: 여행 삭제 API Repository 구현

--- src/main/java/org/triple/backend/travel/repository/TravelItineraryJpaRepository.java
+++ src/main/java/org/triple/backend/travel/repository/TravelItineraryJpaRepository.java
@@ -1,10 +1,15 @@
 package org.triple.backend.travel.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.triple.backend.travel.entity.TravelItinerary;
 
 import java.util.Optional;
 
 public interface TravelItineraryJpaRepository extends JpaRepository<TravelItinerary, Long> {
     Optional<TravelItinerary> findById(Long travelId);
+
+    @Query("select t from TravelItinerary t where t.id = :travelId and t.isDeleted = false")
+    Optional<TravelItinerary> findByIdAndIsDeletedFalse(Long travelId);
 }
