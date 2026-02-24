a7d971df431130b81693e779f0c14f5044574884
sangyunpark99 <psangyun5108@gmail.com>
2026-02-24 23:21:06 +0900

    [#71] feat: Like 연산자를 활용한 조회 제거

--- src/main/java/org/triple/backend/group/repository/GroupJpaRepository.java
+++ src/main/java/org/triple/backend/group/repository/GroupJpaRepository.java
@@ -1,7 +1,6 @@
 package org.triple.backend.group.repository;
 
 import jakarta.persistence.LockModeType;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
@@ -28,27 +27,6 @@ public interface GroupJpaRepository extends JpaRepository<Group, Long> {
     @Lock(LockModeType.PESSIMISTIC_READ)
     @Query("SELECT g FROM Group g WHERE g.id = :groupId")
     Optional<Group> findByIdForRead(Long groupId);
-
-    @Query("""
-            SELECT g
-            FROM Group g
-            WHERE g.groupKind = :kind
-              AND (g.name LIKE CONCAT(:keyword, '%')
-                OR g.description LIKE CONCAT('%', :keyword, '%'))
-            ORDER BY g.id DESC
-            """)
-    List<Group> findFirstPageByKeywordLike(String keyword, Pageable pageable, GroupKind kind);
-
-    @Query("""
-            SELECT g
-            FROM Group g
-            WHERE g.id < :cursor
-              AND g.groupKind = :kind
-              AND (g.name LIKE CONCAT(:keyword, '%')
-                OR g.description LIKE CONCAT('%', :keyword, '%'))
-            ORDER BY g.id DESC
-            """)
-    List<Group> findNextPageByKeywordLike(String keyword, Long cursor, Pageable pageable, GroupKind kind);
 
     @Query(value = """
             SELECT g.*
