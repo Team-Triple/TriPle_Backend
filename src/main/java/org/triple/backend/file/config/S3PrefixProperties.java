16f49ea89ee572a15bc64e558d61037a5a496abc
chungjeongsu <wjdwlgh2000@gmail.com>
2026-02-24 14:07:14 +0900

    [#75] refactor: yml bucket 접근 prefix 추가

--- src/main/java/org/triple/backend/file/config/S3PrefixProperties.java
+++ src/main/java/org/triple/backend/file/config/S3PrefixProperties.java
@@ -11,8 +11,12 @@ public class S3PrefixProperties {
     @NotBlank
     private final String uploaded;
 
-    public S3PrefixProperties(String pending, String uploaded) {
+    @NotBlank
+    private final String getUrlPrefix;
+
+    public S3PrefixProperties(String pending, String uploaded, String getUrlPrefix) {
         this.pending = pending;
         this.uploaded = uploaded;
+        this.getUrlPrefix = getUrlPrefix;
     }
 }
