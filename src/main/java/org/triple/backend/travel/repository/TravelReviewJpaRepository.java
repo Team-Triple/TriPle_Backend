9954faaba0108a6c6e380e4ada46cd590c10351d
sangyunpark99 <psangyun5108@gmail.com>
2026-02-25 23:05:33 +0900

    [#95] feat: 그룹 아이디로 여행 리뷰 조회하는repository 메서드 구현

---
+++ src/main/java/org/triple/backend/travel/repository/TravelReviewJpaRepository.java
@@ -0,0 +1,21 @@
+package org.triple.backend.travel.repository;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.triple.backend.travel.entity.TravelReview;
+
+import java.util.List;
+
+public interface TravelReviewJpaRepository extends JpaRepository<TravelReview, Long> {
+
+    @Query("""
+            SELECT tr
+            FROM TravelReview tr
+            JOIN FETCH tr.user u
+            WHERE tr.travelItinerary.group.id = :groupId
+              AND tr.isDeleted = false
+            ORDER BY tr.id DESC
+            """)
+    List<TravelReview> findRecentByGroupId(Long groupId, Pageable pageable);
+}
