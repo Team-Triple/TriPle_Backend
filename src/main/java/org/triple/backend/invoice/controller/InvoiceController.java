45fecdd5e6519fc827fdc01a784272699adfe323
sangyunpark99 <psangyun5108@gmail.com>
2026-02-25 16:54:26 +0900

    [#91] feat: 청구서 생성 Controller 코드 구현

---
+++ src/main/java/org/triple/backend/invoice/controller/InvoiceController.java
@@ -0,0 +1,27 @@
+package org.triple.backend.invoice.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.triple.backend.auth.session.LoginRequired;
+import org.triple.backend.auth.session.LoginUser;
+import org.triple.backend.invoice.dto.request.InvoiceCreateRequestDto;
+import org.triple.backend.invoice.dto.response.InvoiceCreateResponseDto;
+import org.triple.backend.invoice.service.InvoiceService;
+
+@RestController
+@RequestMapping("/invoices")
+@RequiredArgsConstructor
+public class InvoiceController {
+
+    private final InvoiceService invoiceService;
+
+    @LoginRequired
+    @PostMapping
+    public InvoiceCreateResponseDto create(@LoginUser final Long userId, @RequestBody @Valid InvoiceCreateRequestDto invoiceCreateRequestDto) {
+        return invoiceService.create(userId, invoiceCreateRequestDto);
+    }
+}
