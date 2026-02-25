b954ea9b7cbc3c43e7875472418e8231cf3a9162
sangyunpark99 <psangyun5108@gmail.com>
2026-02-25 18:04:54 +0900

    [#85] chore: Group 엔티티 import 추가

--- src/main/java/org/triple/backend/group/repository/UserGroupJpaRepository.java
+++ src/main/java/org/triple/backend/group/repository/UserGroupJpaRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.triple.backend.group.entity.group.Group;
 import org.triple.backend.group.entity.userGroup.JoinStatus;
 import org.triple.backend.group.entity.userGroup.Role;
 import org.triple.backend.group.entity.userGroup.UserGroup;
