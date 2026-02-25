bac1b9620d664eb798a2569ce9b9d60ba39eb5bd
sangyunpark99 <psangyun5108@gmail.com>
2026-02-25 23:00:51 +0900

    [#95] feat: 그룹 상세 조회 응답값 수정
    - 여행 사진, 여행 리뷰, 여행 일정

--- src/main/java/org/triple/backend/group/dto/response/GroupDetailResponseDto.java
+++ src/main/java/org/triple/backend/group/dto/response/GroupDetailResponseDto.java
@@ -5,16 +5,21 @@ import org.triple.backend.group.entity.group.GroupKind;
 import org.triple.backend.group.entity.userGroup.Role;
 import org.triple.backend.group.entity.userGroup.UserGroup;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record GroupDetailResponseDto(
-    List<UserDto> users,
-    String name,
-    String description,
-    GroupKind groupKind,
-    String thumbNailUrl,
-    int currentMemberCount,
-    int memberLimit
+        List<UserDto> users,
+        String name,
+        String description,
+        GroupKind groupKind,
+        String thumbNailUrl,
+        int currentMemberCount,
+        int memberLimit,
+        Boolean isOwner,
+        List<RecentPhotoDto> photos,
+        List<RecentTravelDto> travels,
+        List<RecentReviewDto> reviews
 ) {
 
     public record UserDto(
@@ -22,16 +27,43 @@ public record GroupDetailResponseDto(
             String description,
             String profileUrl,
             Boolean isOwner
+    ) {}
+
+    public record RecentPhotoDto(
+            Long imageId,
+            String imageUrl
+    ) {}
+
+    public record RecentTravelDto(
+            Long travelItineraryId,
+            String title,
+            String thumbnailUrl,
+            String description,
+            int memberCount,
+            int memberLimit,
+            LocalDateTime startAt,
+            LocalDateTime endAt
+    ) {}
+
+    public record RecentReviewDto(
+            Long reviewId,
+            String content,
+            String writerNickname
+    ) {}
+
+    public static GroupDetailResponseDto from(
+            final List<UserGroup> userGroups,
+            final Group group,
+            final Role role,
+            final List<RecentPhotoDto> recentPhotos,
+            final List<RecentTravelDto> recentTravels,
+            final List<RecentReviewDto> recentReviews
     ) {
-
-    }
-
-    public static GroupDetailResponseDto from(final List<UserGroup> userGroups, final Group group) {
-        List<UserDto> users= userGroups.stream().map(ug -> new UserDto(
+        List<UserDto> users = userGroups.stream().map(ug -> new UserDto(
                 ug.getUser().getNickname(),
                 ug.getUser().getDescription(),
                 ug.getUser().getProfileUrl(),
-                ug.getRole().equals(Role.OWNER)
+                ug.getRole() == Role.OWNER
         )).toList();
 
         return new GroupDetailResponseDto(
@@ -41,7 +73,11 @@ public record GroupDetailResponseDto(
                 group.getGroupKind(),
                 group.getThumbNailUrl(),
                 group.getCurrentMemberCount(),
-                group.getMemberLimit()
+                group.getMemberLimit(),
+                role == Role.OWNER,
+                recentPhotos,
+                recentTravels,
+                recentReviews
         );
     }
 }
