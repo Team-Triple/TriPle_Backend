b3b7dd0c6cfa488b3cece8ae6549eb1c733eb168
sangyunpark99 <psangyun5108@gmail.com>
2026-02-25 12:06:57 +0900

    [#87] feat: GroupErrorCode 항목 추가
    - 그룹 주인은 추방할 수 없습니다.
    - 자기 자신은 추방할 수 없습니다.

--- src/main/java/org/triple/backend/group/exception/GroupErrorCode.java
+++ src/main/java/org/triple/backend/group/exception/GroupErrorCode.java
@@ -9,7 +9,9 @@ public enum GroupErrorCode implements ErrorCode {
     NOT_GROUP_OWNER(HttpStatus.FORBIDDEN, "그룹 수정/삭제 권한이 없습니다."),
     CONCURRENT_GROUP_UPDATE(HttpStatus.CONFLICT, "동시에 그룹 정보가 변경되었습니다. 다시 시도해주세요."),
     EXCEEDED_JOIN_NUMBER(HttpStatus.CONFLICT, "그룹 정원이 가득 찼습니다."),
-    NOT_GROUP_MEMBER(HttpStatus.FORBIDDEN, "해당 그룹을 조회할 권한이 없습니다.");
+    NOT_GROUP_MEMBER(HttpStatus.FORBIDDEN, "해당 그룹을 조회할 권한이 없습니다."),
+    CANNOT_KICK_OWNER(HttpStatus.FORBIDDEN, "그룹 주인은 추방할 수 없습니다."),
+    CANNOT_KICK_SELF(HttpStatus.FORBIDDEN, "자기 자신은 추방할 수 없습니다.");
 
     private HttpStatus status;
     private String message;
