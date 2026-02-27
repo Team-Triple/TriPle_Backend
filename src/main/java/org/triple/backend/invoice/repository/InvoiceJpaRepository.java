92e7efd19beb41c89e75d86d051728e676cbf322
chungjeongsu <wjdwlgh2000@gmail.com>
2026-02-27 18:50:51 +0900

    [#115] feat: 청구서 조회 API Repository 구현

--- src/main/java/org/triple/backend/invoice/repository/InvoiceJpaRepository.java
+++ src/main/java/org/triple/backend/invoice/repository/InvoiceJpaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.triple.backend.invoice.entity.Invoice;
+import org.triple.backend.invoice.entity.InvoiceStatus;
 
 import java.util.Optional;
 
@@ -16,4 +17,18 @@ public interface InvoiceJpaRepository extends JpaRepository<Invoice, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select i from Invoice i where i.id = :invoiceId")
     Optional<Invoice> findByIdForUpdate(Long invoiceId);
+
+    @Query("""
+            select distinct i
+            from Invoice i
+            join fetch i.creator
+            left join fetch i.invoiceUsers iu
+            left join fetch iu.user
+            where i.travelItinerary.id = :travelItineraryId
+              and i.invoiceStatus <> :invoiceStatus
+            """)
+    Optional<Invoice> findInvoiceDetailByTravelItineraryIdAndInvoiceStatusNot(
+            Long travelItineraryId,
+            InvoiceStatus invoiceStatus
+    );
 }
