843f83f8f08f2418d6eb81cb3e5f66c3e478e5d9
sangyunpark99 <psangyun5108@gmail.com>
2026-02-27 21:46:16 +0900

    [#114] feat: 인증서 수정 응답 dto 구현

---
+++ src/main/java/org/triple/backend/invoice/dto/response/InvoiceAdjustResponseDto.java
@@ -0,0 +1,30 @@
+package org.triple.backend.invoice.dto.response;
+
+import org.triple.backend.invoice.dto.RecipientAmountDto;
+import org.triple.backend.invoice.entity.Invoice;
+import org.triple.backend.invoice.entity.InvoiceStatus;
+import org.triple.backend.invoice.entity.InvoiceUser;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record InvoiceAdjustResponseDto(
+        Long invoiceId,
+        BigDecimal totalAmount,
+        List<RecipientAmountDto> recipients,
+        InvoiceStatus invoiceStatus
+) {
+
+    public static InvoiceAdjustResponseDto from(final Invoice invoice, final List<InvoiceUser> invoiceUsers) {
+        List<RecipientAmountDto> recipients = invoiceUsers.stream()
+                .map(invoiceUser -> new RecipientAmountDto(invoiceUser.getUser().getId(), invoiceUser.getRemainAmount()))
+                .toList();
+
+        return new InvoiceAdjustResponseDto(
+                invoice.getId(),
+                invoice.getTotalAmount(),
+                recipients,
+                invoice.getInvoiceStatus()
+        );
+    }
+}
