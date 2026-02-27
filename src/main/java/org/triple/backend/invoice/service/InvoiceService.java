43bd62e188e43e149fae82748fd403275000d7dd
chungjeongsu <wjdwlgh2000@gmail.com>
2026-02-27 16:36:55 +0900

    [#111] refactor: 필요없는 flush 삭제

--- src/main/java/org/triple/backend/invoice/service/InvoiceService.java
+++ src/main/java/org/triple/backend/invoice/service/InvoiceService.java
@@ -168,6 +168,5 @@ public class InvoiceService {
 
         invoiceUserJpaRepository.deleteAllByInvoiceIdInBatch(invoiceId);
         invoice.markDeleted();
-        invoiceRepository.flush();
     }
 }
