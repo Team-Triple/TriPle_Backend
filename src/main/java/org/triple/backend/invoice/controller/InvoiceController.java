61876f3f079b374ac18579073070a74c36c081d4
sangyunpark99 <psangyun5108@gmail.com>
2026-02-27 15:32:23 +0900

    [#110] feat: 청구서 수정 API Controller 구현

--- src/main/java/org/triple/backend/invoice/controller/InvoiceController.java
+++ src/main/java/org/triple/backend/invoice/controller/InvoiceController.java
@@ -2,14 +2,13 @@ package org.triple.backend.invoice.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.triple.backend.auth.session.LoginRequired;
 import org.triple.backend.auth.session.LoginUser;
 import org.triple.backend.invoice.dto.request.InvoiceCreateRequestDto;
+import org.triple.backend.invoice.dto.request.InvoiceUpdateRequestDto;
 import org.triple.backend.invoice.dto.response.InvoiceCreateResponseDto;
+import org.triple.backend.invoice.dto.response.InvoiceUpdateResponseDto;
 import org.triple.backend.invoice.service.InvoiceService;
 
 @RestController
@@ -24,4 +23,11 @@ public class InvoiceController {
     public InvoiceCreateResponseDto create(@LoginUser final Long userId, @RequestBody @Valid InvoiceCreateRequestDto invoiceCreateRequestDto) {
         return invoiceService.create(userId, invoiceCreateRequestDto);
     }
+
+    @LoginRequired
+    @PatchMapping("/{invoiceId}")
+    public InvoiceUpdateResponseDto updateMetaInfo(@LoginUser final Long userId, @PathVariable Long invoiceId, @RequestBody @Valid InvoiceUpdateRequestDto dto) {
+        return invoiceService.updateMetaInfo(userId, invoiceId, dto);
+    }
+
 }
