00336e68a3bdd4a5e28b5650620893dec5403e6f
chungjeongsu <wjdwlgh2000@gmail.com>
2026-02-27 15:40:35 +0900

    [#111] feat: 청구서 삭제 API Repository 구현
    - 소프트 딜리트 업데이트를 위한 비관 락

--- src/main/java/org/triple/backend/invoice/repository/InvoiceJpaRepository.java
+++ src/main/java/org/triple/backend/invoice/repository/InvoiceJpaRepository.java
@@ -1,7 +1,16 @@
 package org.triple.backend.invoice.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.triple.backend.invoice.entity.Invoice;
 
+import java.util.Optional;
+
 public interface InvoiceJpaRepository extends JpaRepository<Invoice, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select i from Invoice i where i.id = :invoiceId")
+    Optional<Invoice> findByIdForUpdate(Long invoiceId);
 }
--- src/main/java/org/triple/backend/invoice/repository/InvoiceUserJpaRepository.java
+++ src/main/java/org/triple/backend/invoice/repository/InvoiceUserJpaRepository.java
@@ -1,7 +1,14 @@
 package org.triple.backend.invoice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.triple.backend.invoice.entity.InvoiceUser;
 
 public interface InvoiceUserJpaRepository extends JpaRepository<InvoiceUser, Long> {
+
+    @Modifying(flushAutomatically = true)
+    @Query("delete from InvoiceUser iu where iu.invoice.id = :invoiceId")
+    void deleteAllByInvoiceIdInBatch(@Param("invoiceId") Long invoiceId);
 }
