11537d4a490997dc7021c8362267f4d925c1c6ad
sangyunpark99 <psangyun5108@gmail.com>
2026-03-02 16:39:33 +0900

    [#124] feat: 사용자 아이디, 청구서 아이디,청구서 상태 값으로 비관락을 거는 조회 메서드 구현

--- src/main/java/org/triple/backend/invoice/repository/InvoiceUserJpaRepository.java
+++ src/main/java/org/triple/backend/invoice/repository/InvoiceUserJpaRepository.java
@@ -1,14 +1,27 @@
 package org.triple.backend.invoice.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.triple.backend.invoice.entity.InvoiceStatus;
 import org.triple.backend.invoice.entity.InvoiceUser;
+
+import java.util.Optional;
 
 public interface InvoiceUserJpaRepository extends JpaRepository<InvoiceUser, Long> {
 
     @Modifying(flushAutomatically = true)
     @Query("delete from InvoiceUser iu where iu.invoice.id = :invoiceId")
     void deleteAllByInvoiceIdInBatch(@Param("invoiceId") Long invoiceId);
+
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT iu FROM InvoiceUser iu JOIN FETCH iu.user u WHERE u.id =:userId AND iu.invoice.id =:invoiceId AND iu.invoice.invoiceStatus = :invoiceStatus")
+    Optional<InvoiceUser> findByUserIdAndInvoiceIdAndInvoiceStatusForUpdate(
+            Long userId,
+            Long invoiceId,
+            InvoiceStatus invoiceStatus
+    );
 }
