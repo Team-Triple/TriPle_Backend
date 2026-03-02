85be380726a0bb344e707f6bd1d5911f4894a937
sangyunpark99 <psangyun5108@gmail.com>
2026-03-02 16:41:26 +0900

    [#124] feat: 결제 에러 코드 구현

---
+++ src/main/java/org/triple/backend/payment/exception/PaymentErrorCode.java
@@ -0,0 +1,31 @@
+package org.triple.backend.payment.exception;
+
+import org.springframework.http.HttpStatus;
+import org.triple.backend.global.error.ErrorCode;
+
+public enum PaymentErrorCode implements ErrorCode {
+
+    DUPLICATED_PAYMENT(HttpStatus.CONFLICT, "중복된 결제 생성 요청입니다."),
+    PAYMENT_ALREADY_COMPLETED(HttpStatus.CONFLICT, "이미 결제가 완료된 청구 대상입니다."),
+    PAYMENT_AMOUNT_EXCEEDS_REMAINING(HttpStatus.CONFLICT, "요청 결제 금액이 남은 금액을 초과합니다."),
+    PAYMENT_NOT_ALLOWED(HttpStatus.FORBIDDEN, "결제를 진행할 수 없는 청구서입니다."),
+    PAYMENT_ALREADY_IS_ACTIVE(HttpStatus.FORBIDDEN, "이미 실행중인 결제입니다.");
+
+    PaymentErrorCode(final HttpStatus status, final String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    private HttpStatus status;
+    private String message;
+
+    @Override
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
