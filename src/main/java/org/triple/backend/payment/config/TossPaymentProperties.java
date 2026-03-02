f834d6de9ff2be3b1cd494d2e455b3b14d12ba6e
jeongjiho <wjdwlgh2000@naver.com>
2026-03-02 21:54:25 +0900

    [#130] feat: 결제 승인 API Config 구현

---
+++ src/main/java/org/triple/backend/payment/config/TossPaymentProperties.java
@@ -0,0 +1,10 @@
+package org.triple.backend.payment.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "toss.payment")
+public record TossPaymentProperties(
+    String uri,
+    String secret,
+    String contentType
+) {}
