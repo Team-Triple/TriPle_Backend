1d29158780ca016fd56d3de32a5285994ac49dd7
sangyunpark99 <psangyun5108@gmail.com>
2026-03-02 16:40:47 +0900

    [#124] feat: 결제 생성 API Controller 구현

---
+++ src/main/java/org/triple/backend/payment/controller/PaymentController.java
@@ -0,0 +1,25 @@
+package org.triple.backend.payment.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.triple.backend.auth.session.LoginRequired;
+import org.triple.backend.auth.session.LoginUser;
+import org.triple.backend.payment.dto.request.PaymentCreateReq;
+import org.triple.backend.payment.dto.response.PaymentCreateRes;
+import org.triple.backend.payment.service.PaymentService;
+
+@RestController
+@RequestMapping("/payments")
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @LoginRequired
+    @PostMapping("/{invoiceId}")
+    public PaymentCreateRes create(@Valid @RequestBody final PaymentCreateReq paymentCreateReq,
+                                   @PathVariable final Long invoiceId, @LoginUser Long userId) {
+        return paymentService.create(paymentCreateReq, invoiceId, userId);
+    }
+}
