8e745905f594ccf5e81253e0d38b726dff41eb9d
jeongjiho <wjdwlgh2000@naver.com>
2026-03-02 21:55:54 +0900

    [#130] feat: 결제 승인 API Repository 구현

--- src/main/java/org/triple/backend/payment/repository/PaymentJpaRepository.java
+++ src/main/java/org/triple/backend/payment/repository/PaymentJpaRepository.java
@@ -1,9 +1,22 @@
 package org.triple.backend.payment.repository;
 
+import jakarta.persistence.LockModeType;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.triple.backend.payment.entity.Payment;
 
 public interface PaymentJpaRepository extends JpaRepository<Payment, Long> {
 
     boolean existsByInvoiceId(Long invoiceId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+    SELECT p
+    FROM Payment p
+    WHERE p.orderId = :orderId
+    """)
+    Optional<Payment> findByOrderId(@Param("orderId") String orderId);
 }
