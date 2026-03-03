0c7a21a6fd9112b0b3be623c8090fb104c36c0c8
sangyunpark99 <psangyun5108@gmail.com>
2026-03-03 11:19:28 +0900

    [#128] feat: 결제 목록 조회 쿼리 구현

--- src/main/java/org/triple/backend/payment/repository/PaymentJpaRepository.java
+++ src/main/java/org/triple/backend/payment/repository/PaymentJpaRepository.java
@@ -1,10 +1,14 @@
 package org.triple.backend.payment.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.triple.backend.payment.entity.Payment;
 import org.triple.backend.payment.entity.PaymentStatus;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface PaymentJpaRepository extends JpaRepository<Payment, Long> {
 
@@ -15,4 +19,27 @@ public interface PaymentJpaRepository extends JpaRepository<Payment, Long> {
             Long userId,
             Collection<PaymentStatus> paymentStatuses
     );
+
+    @Query("SELECT p FROM Payment p ORDER BY p.id desc")
+    List<Payment> findFirstPage(Pageable pageable);
+
+    @Query("SELECT p FROM Payment p WHERE p.id < :cursor ORDER BY p.id desc")
+    List<Payment> findNextPage(Long cursor, Pageable pageable);
+
+    @Query(value = """
+            SELECT p.*
+            FROM payment p
+            WHERE MATCH(p.name) AGAINST(:booleanQuery IN BOOLEAN MODE)
+            ORDER BY p.payment_id DESC
+            """, nativeQuery = true)
+    List<Payment> findFirstPageByKeywordFullText(String booleanQuery, Pageable pageable);
+
+    @Query(value = """
+            SELECT p.*
+            FROM payment p
+            WHERE p.payment_id < :cursor
+              AND MATCH(p.name) AGAINST(:booleanQuery IN BOOLEAN MODE)
+            ORDER BY p.payment_id DESC
+            """, nativeQuery = true)
+    List<Payment> findNextPageByKeywordFullText(String booleanQuery, Long cursor, Pageable pageable);
 }
