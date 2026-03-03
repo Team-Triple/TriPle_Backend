7d9e47a6cd86721ab06a7d14107832c9f95ca114
sangyunpark99 <psangyun5108@gmail.com>
2026-03-03 11:17:36 +0900

    [#128] feat: 결제 목록 조회 API Controller 구현
    - 키워드, 커서 방식 활용

--- src/main/java/org/triple/backend/payment/controller/PaymentController.java
+++ src/main/java/org/triple/backend/payment/controller/PaymentController.java
@@ -7,6 +7,7 @@ import org.triple.backend.auth.session.LoginRequired;
 import org.triple.backend.auth.session.LoginUser;
 import org.triple.backend.payment.dto.request.PaymentCreateReq;
 import org.triple.backend.payment.dto.response.PaymentCreateRes;
+import org.triple.backend.payment.dto.response.PaymentCursorRes;
 import org.triple.backend.payment.service.PaymentService;
 
 @RestController
@@ -21,5 +22,13 @@ public class PaymentController {
     public PaymentCreateRes create(@Valid @RequestBody final PaymentCreateReq paymentCreateReq,
                                    @PathVariable final Long invoiceId, @LoginUser Long userId) {
         return paymentService.create(paymentCreateReq, invoiceId, userId);
+    }
+
+    @LoginRequired
+    @GetMapping
+    public PaymentCursorRes search(@RequestParam(required = false) String keyword,
+                                   @RequestParam(required = false) Long cursor,
+                                   @RequestParam(defaultValue = "10") int size) {
+        return paymentService.search(keyword, cursor, size);
     }
 }
