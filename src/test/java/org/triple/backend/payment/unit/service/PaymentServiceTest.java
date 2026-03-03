8e206e70f7798d8a428c1e4ff2db468ad39b1b38
sangyunpark99 <psangyun5108@gmail.com>
2026-03-03 14:38:29 +0900

    [#129] test: Payment 엔티티 name 필드 제거 테스트 코드 적용

--- src/test/java/org/triple/backend/payment/unit/service/PaymentServiceTest.java
+++ src/test/java/org/triple/backend/payment/unit/service/PaymentServiceTest.java
@@ -190,7 +190,6 @@ class PaymentServiceTest {
                         .invoice(invoice)
                         .user(payer)
                         .pgProvider(PgProvider.TOSS)
-                        .name("기존 진행중 결제")
                         .method(PaymentMethod.TRANSFER)
                         .orderId(UUID.randomUUID().toString())
                         .requestedAmount(new BigDecimal("1000"))
