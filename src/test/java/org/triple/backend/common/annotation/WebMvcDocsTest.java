5adea111c05ada07b427f1736cd9aee08bc1ccc7
chungjeongsu <wjdwlgh2000@gmail.com>
2026-03-04 16:29:16 +0900

    [#140] refactor: test.yml 설정 추가 및 테스트 properties 추가

--- src/test/java/org/triple/backend/common/annotation/WebMvcDocsTest.java
+++ src/test/java/org/triple/backend/common/annotation/WebMvcDocsTest.java
@@ -2,9 +2,11 @@ package org.triple.backend.common.annotation;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.ActiveProfiles;
+import org.triple.backend.auth.config.property.CookieProperties;
 import org.triple.backend.auth.cookie.CookieManager;
 import org.triple.backend.auth.session.SessionManager;
 import org.triple.backend.auth.session.UserIdentityResolver;
@@ -17,6 +19,7 @@ import java.lang.annotation.*;
 @Documented
 @ExtendWith(RestDocumentationExtension.class)
 @Import({SessionManager.class, UserIdentityResolver.class, UuidCrypto.class, CookieManager.class})
+@EnableConfigurationProperties(CookieProperties.class)
 @WebMvcTest
 @ActiveProfiles("test")
 public @interface WebMvcDocsTest {
