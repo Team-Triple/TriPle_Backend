8c4cc0256c0aefa759ca7ae32d2337c76a2918b0
sangyunpark99 <psangyun5108@gmail.com>
2026-03-04 09:14:48 +0900

    [#137] feat: 로그인 응답값에 publicUuid 반환

--- src/main/java/org/triple/backend/auth/dto/response/AuthLoginResponseDto.java
+++ src/main/java/org/triple/backend/auth/dto/response/AuthLoginResponseDto.java
@@ -1,6 +1,7 @@
 package org.triple.backend.auth.dto.response;
 
 public record AuthLoginResponseDto(
+        String publicUuid,
         String nickname,
         String email,
         String profileUrl
