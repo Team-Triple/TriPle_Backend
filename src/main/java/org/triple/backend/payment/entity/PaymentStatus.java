c17f5ec24a139609ce2a4606fab0aa685ae7bf40
chungjeongsu <wjdwlgh2000@gmail.com>
2026-03-04 21:34:47 +0900

    [#145] feat: 결제 승인 API entity 로직 추가

--- src/main/java/org/triple/backend/payment/entity/PaymentStatus.java
+++ src/main/java/org/triple/backend/payment/entity/PaymentStatus.java
@@ -1,5 +1,5 @@
 package org.triple.backend.payment.entity;
 
 public enum PaymentStatus {
-    READY, IN_PROGRESS, DONE, CANCELED, ABORTED, EXPIRED
+    READY, IN_PROGRESS, DONE, CANCELED, RETRY_FAILED, FAILED, DB_FAILED
 }
